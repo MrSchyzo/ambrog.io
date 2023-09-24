996d9d2d3cdb9de50269202f214aac1757c8b15d
Marco <16671172+MrSchyzo@users.noreply.github.com>
2023-09-25 00:15:06 +0200

    Fix CI, hopefully (#48)

--- bin/src/info.rs
+++ bin/src/info.rs
@@ -1,3 +1,5 @@
-// This version is automatically edited in the repo CI
-// please think twice before editing this!
-pub const VERSION: &str = "";
+/* 
+    This version is automatically edited in the repo CI
+    please think twice before editing this!
+*/
+pub const VERSION: &str = "0.1.2";
